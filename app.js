d55bf1e6eb70af176fd0f4f1f177ee38411ff1cd
azaamabubacker <cleancodelk@gmail.com>
2023-01-11 14:24:41 +0530

    seperated the users page and added express routes

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 const app = express();
 
+const usersRoutes = require("./users");
+
 app.use(express.static(path.join(__dirname, "views")));
 
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "./", "views", "index.html"));
 });
 
-app.get("/users", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "./", "views", "users.html"));
-});
+app.use(usersRoutes);
 
 app.listen(3000);
